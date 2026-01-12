6b804515406367463f3b552c203eafbb501f3ef5
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2026-01-12 17:43:46 +0530

    Add action method for predicting field conditions
    Implement action method to predict field conditions using cosine similarity.

--- Environment.py
+++ Environment.py
@@ -1,1 +1,15 @@
+from numpy import dot
+from numpy.linalg import norm
 
+class Environment:
+    # Agent will call environment action function to predict field condition
+    def action(self, train_data, train_label, test_data):
+        label = None
+        score = 0
+        for i in range(0, 100):
+            # Calculate cosine similarity
+            predict = dot(train_data[i], test_data) / (norm(train_data[i]) * norm(test_data))
+            if predict > score:
+                score = predict
+                label = train_label[i]
+        return label
